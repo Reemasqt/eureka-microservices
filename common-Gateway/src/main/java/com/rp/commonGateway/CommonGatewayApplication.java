1b3ffbe49e02735fb154df35e47dbc707001a99b
Reemasqt <reema.prajapati@softqubes.com>
2022-11-04 16:05:59 +0530

    Implemented Hystrix

--- common-Gateway/src/main/java/com/rp/commonGateway/CommonGatewayApplication.java
+++ common-Gateway/src/main/java/com/rp/commonGateway/CommonGatewayApplication.java
@@ -3,9 +3,11 @@ package com.rp.commonGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class CommonGatewayApplication {
 
 	public static void main(String[] args) {
